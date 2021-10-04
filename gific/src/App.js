f1afafe5aa7dbc3ec1249c1c7b38961ba2478865
Yannis Alouache <alouacehyannis1gmail.com>
2021-10-04 20:12:26 +0200

    [+] added Tailwind css && Footer component

--- gific/src/App.js
+++ gific/src/App.js
@@ -1,6 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/pages/Home/Home"
+
 function App() {
   return (
-    <h1>test</h1>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
