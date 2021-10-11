9498f8199887100b8bb58b448db499a0f9d5eb63
Yannis Alouache <alouacehyannis1gmail.com>
2021-10-11 03:40:24 +0200

    [+] added Hero Banner to Home Page

---
+++ gific/src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import image from '../../assets/image/herog.jpg'
+import "./Hero.css"
+
+const Hero = () => {
+    return (
+        <>
+            <section class="relative hero-section flex flex-col items-center justify-center text-center text-white py-0 px-3">
+                <div class="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+                    <img src={image} class="min-w-full min-h-full absolute object-cover"></img>
+                </div>
+                <div class="video-content space-y-2">
+                    <h1 class="text-6xl">GIFIC C'EST VOTRE SOURCE DE GIFS TENDANCE !</h1>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Hero
