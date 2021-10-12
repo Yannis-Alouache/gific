cd523111c2ac378c6738ed3191ac066b21c8237e
Yannis-Alouache <alouacehyannis1@gmail.com>
2021-10-13 00:30:26 +0200

    [+] added Carousel

--- gific/src/components/pages/Home/Home.jsx
+++ gific/src/components/pages/Home/Home.jsx
@@ -3,12 +3,81 @@ import Footer from '../../Footer/Footer'
 import Header from '../../Header/Header'
 import Hero from '../../Hero/Hero'
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const Home = () => {
+
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false
+    };
 
     return (
         <>
             <Header />
             <Hero />
+            <br />
+            <br />
+
+            <div className="container mx-auto">
+                <Slider {...settings}>
+                    <div>
+                        <img className="rounded-lg shadow-lg " src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+                    <div>
+                        <img className="rounded-lg shadow-lg" src="https://media.tenor.com/images/1a6e398331272fbd91c1f5b4d87d4418/tenor.gif" />
+                    </div>
+
+                    
+
+                </Slider>
+            </div>
+            
+
+            <br />
+            <br />
             <Footer />
         </>
     )
